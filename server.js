1d3e32597f6b8b1aaeedb4b812339ad2147d0eb3
Or Goren <orrgn1766@gmail.com>
2026-04-11 22:12:19 +0300

    2

--- server.js
+++ server.js
@@ -24,13 +24,13 @@ mongoose
 //use Routes:
 app.use("/api/items", items);
 
-// serve static assests if we are in production
+// serve static assets if we are in production
 if (process.env.NODE_ENV === "production") {
-  //set static folder:
-  app.use(express.static("client/build"));
+  // set static folder:
+  app.use(express.static(path.join(__dirname, "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
